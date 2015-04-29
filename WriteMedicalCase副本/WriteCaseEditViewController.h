623b06e20e231f021ac36e8294f2be0959c04275
GK <GK@GK-2.local>
2015-04-30 00:23:13 +0800

    first commit

---
+++ WriteMedicalCase副本/WriteCaseEditViewController.h
@@ -0,0 +1,13 @@
+//
+//  WriteCaseEditViewController.h
+//  WriteMedicalCase
+//
+//  Created by GK on 15/4/29.
+//  Copyright (c) 2015年 GK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WriteCaseEditViewController : UIViewController
+
+@end
